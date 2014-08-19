090abfeab6a9e748c265f878d512b65935a548e9
Nick <nick@webstudioswest.com>
2014-08-19 10:26:43 -0700

    add auto send email template

--- QuestaVoltaPerfectServer.sh
+++ QuestaVoltaPerfectServer.sh
@@ -484,6 +484,36 @@ install_unzip(){
   yum install unzip bzip2 unrar perl-DBD-mysql -y >> $LOG 2>&1
 }
 
+send_credentials(){
+$to = 'bob@example.com';
+
+$subject = 'Website Change Request';
+
+$headers = "From: " . strip_tags($_POST['req-email']) . "\r\n";
+$headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
+$headers .= "CC: susan@example.com\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$message = '<html><body>';
+$message .= '<img src="http://css-tricks.com/examples/WebsiteChangeRequestForm/images/wcrf-header.png" alt="Website Change Request" />';
+$message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+$message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . strip_tags($_POST['req-name']) . "</td></tr>";
+$message .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_POST['req-email']) . "</td></tr>";
+$message .= "<tr><td><strong>Type of Change:</strong> </td><td>" . strip_tags($_POST['typeOfChange']) . "</td></tr>";
+$message .= "<tr><td><strong>Urgency:</strong> </td><td>" . strip_tags($_POST['urgency']) . "</td></tr>";
+$message .= "<tr><td><strong>URL To Change (main):</strong> </td><td>" . $_POST['URL-main'] . "</td></tr>";
+$addURLS = $_POST['addURLS'];
+if (($addURLS) != '') {
+    $message .= "<tr><td><strong>URL To Change (additional):</strong> </td><td>" . strip_tags($addURLS) . "</td></tr>";
+}
+$curText = htmlentities($_POST['curText']);           
+if (($curText) != '') {
+    $message .= "<tr><td><strong>CURRENT Content:</strong> </td><td>" . $curText . "</td></tr>";
+}
+$message .= "<tr><td><strong>NEW Content:</strong> </td><td>" . htmlentities($_POST['newText']) . "</td></tr>";
+$message .= "</table>";
+$message .= "</body></html>";
+}
 
 disable_fw
 disable_selinux
@@ -510,6 +540,8 @@ install_cyrus
 install_bind
 configure_webdav
 install_squirrelmail
+
+
 
 echo -e "[\033[33m*\033[0m] Setting up ISPConfig !"
 #ISPConfig
